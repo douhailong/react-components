54d10152ed25bc31f122fee71345a6f103b64c00
douhailong <1804610117@qq.com>
2024-05-31 00:55:12 +0800

    color

--- stories/button.story.tsx
+++ stories/button.story.tsx
@@ -2,19 +2,39 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 import Button from '@components/button';
 
-const meta = {
-  title: 'Example/Button',
-  component: Button,
-  parameters: {},
-  tags: ['autodocs']
-} satisfies Meta<typeof Button>;
-
-export default meta;
-type Story = StoryObj<typeof meta>;
-
-export const Primary: Story = {
-  args: {
-    children: <>jjkkkkkkkk</>
-    // label: 'Button'
-  }
+export const Status = () => {
+  return (
+    <div>
+      <Button>Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button>Danger Button</Button>
+      <Button>Disabled Button</Button>
+      <Button>Default Button</Button>
+    </div>
+  );
 };
+
+export const Demo1 = () => {
+  return <div>kkk</div>;
+};
+
+export default {
+  title: 'Buttons'
+};
+
+// const meta = {
+//   title: 'Example/Button',
+//   component: Demo,
+//   parameters: {},
+//   tags: ['autodocs']
+// } satisfies Meta<typeof Demo>;
+
+// export default meta;
+// type Story = StoryObj<typeof meta>;
+
+// export const Primary: Story = {
+//   // args: {
+//   //   // children: <>jjkkkkkkkk</>
+//   //   // label: 'Button'
+//   // }
+// };
